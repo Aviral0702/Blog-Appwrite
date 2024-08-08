68cd86de938c5edea9011d2a3f9c93d27d15dbaa
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-08-08 22:03:05 +0530

    feat: Add navigation menu to Header component

---
+++ src/Component/Header/Logout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authService from '../../Appwrite/Config'
+import {logout} from '../../store/authSlice'
+function LogoutBtn() {
+    const dispatch = useDispatch();
+    const logoutHandler = () => {
+        authService.logout().then(()=>{
+            dispatch(logout())
+        })
+    }
+    return (
+        <button
+            className=''
+
+        >
+            Logout
+        </button>
+    )
+}
+
+export default LogoutBtn
