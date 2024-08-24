572730623aa0de776d6890837127fd60cbe55f3a
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-08-24 23:22:25 +0530

    feat: Add routing for different pages in the application

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,79 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from "./store/store.js"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import { AuthLayout, Login, SignUp } from './Component/index.js'
+import AllPost from './Pages/AllPost.jsx'
+import AddPost from './Pages/AddPost.jsx'
+import EditPost from './Pages/EditPost.jsx'
+import Post from './Pages/Post.jsx'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children:[
+      {
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path:'/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <SignUp/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/all-post',
+        element : (
+          <AuthLayout authentication={false}>
+            {" "}
+            <AllPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/add-post',
+        element: (
+          <AuthLayout authentication={true}>
+            {" "}
+            <AddPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/edit-post/:slug',
+        element:(
+          <AuthLayout authentication={true}>
+            {" "}
+            <EditPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/post/:slug',
+        element: <Post/>
+      }
+
+    ]
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 )
